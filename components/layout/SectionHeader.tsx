a16bbb6cba89f3222eeb0d1d961816c998b92a16
Muhammad Ilham Setiawan <m.ilsetiawan1@gmail.com>
2026-05-26 17:35:49 +0700

    refactor(ui): update SectionHeader subtitle to support ReactNode

--- components/layout/SectionHeader.tsx
+++ components/layout/SectionHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface SectionHeaderProps {
   title: string;
-  subtitle?: string;
+  subtitle?: React.ReactNode;
   align?: "left" | "center";
   className?: string;
 }
